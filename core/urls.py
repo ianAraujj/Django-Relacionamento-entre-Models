c18867c7739c23a1d149eb53cff63485da716a68
ianAraujj <ianlucas2503@gmail.com>
2020-08-09 20:44:51 -0300

    Criando as rotas usando a APIView

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from core.views import ListarAlunos, ObterAluno, AlteraAluno
+from core.views import Alunos
 
 urlpatterns = [
-    path('alunos/', ListarAlunos.as_view(), name="listar-alunos"),
-    path('aluno/',ObterAluno.as_view(), name="Obter-aluno"),
-    path('aluno/alterar', AlteraAluno.as_view(), name="Alterar Aluno")
+    path('alunos/', Alunos.as_view(), name="listar-alunos"),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render
-from rest_framework import generics
+# rest_framework
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+
+# core
 from core.serializers import AlunoSerializer
 from core.models import Aluno
 
-class ListarAlunos(generics.ListCreateAPIView):
 
-    queryset = Aluno.objects.all()
+class Alunos(APIView):
+    parser_classes = (MultiPartParser, FormParser,)
     serializer_class = AlunoSerializer
 
-class ObterAluno(generics.ListCreateAPIView):
+    def get(self, request, format=None):
+        queryset = Aluno.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, format=None):
+        pass
+
+""" class ObterAluno(APIView):
     serializer_class = AlunoSerializer
 
     def get_queryset(self):
@@ -19,7 +33,7 @@ class ObterAluno(generics.ListCreateAPIView):
 
         return Aluno.objects.filter(matricula=matricula)
 
-class AlteraAluno(generics.RetrieveUpdateDestroyAPIView):
+class AlteraAluno(APIView):
 
     serializer_class = AlunoSerializer
 
@@ -35,4 +49,4 @@ class AlteraAluno(generics.RetrieveUpdateDestroyAPIView):
         aluno.nome = novo_nome
         aluno.save()
 
-        return aluno
+        return aluno """
